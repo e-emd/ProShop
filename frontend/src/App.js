d0a326d03e4293ee0059947df971e6d5453accb9
e-emd <dewin.emd@gmail.com>
2020-10-13 21:22:43 -0600

    React redux in frontend with mongo db set up"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './componets/Header';
+import Footer from './componets/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
